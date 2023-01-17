bfb1f36eaefe5b3e778957376af692c4bcc8a52f
Apoorva Arya <88702628+apoorva-aa@users.noreply.github.com>
2023-01-17 23:20:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import EntropyToggler from "./components/EntropyToggler";
 import entropies from "./data/entropies";
 // console.log(entropies);
 
-const lastUpdatedDate = "14-11-2022";
+const lastUpdatedDate = "16-01-2023";
 
 const App = () => {
   const [countrySelection, onCountrySelect] = useState("India");
